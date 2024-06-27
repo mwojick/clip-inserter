27b327c2d07723de65f9df9bb3fd9669d833cd5e
mwojick <35327565+mwojick@users.noreply.github.com>
2024-06-26 22:36:08 -0700

    toggle

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	let currentTab: chrome.tabs.Tab | null = $state(null);
-	let options = $state({});
+	let currentUrl: string = $state('');
+	let options: { allowedURL: string; popupTabId: number | null } = $state({
+		allowedURL: '',
+		popupTabId: null
+	});
 
-	$inspect('currentTab', currentTab);
+	let isClipEnabled = $derived(options.allowedURL === currentUrl);
+
+	$inspect('currentUrl', currentUrl);
 	$inspect('OPTS', options);
 
 	async function getCurrentTab() {
@@ -13,18 +18,39 @@
 
 	$effect(() => {
 		async function getInitialData() {
-			const [ct, data] = await Promise.all([getCurrentTab(), chrome.storage.sync.get('options')]);
-			currentTab = ct;
-			options = { ...options, ...data.options };
+			const [ct, data] = await Promise.all([getCurrentTab(), chrome.storage.local.get('options')]);
+			const tabId = ct?.id || null;
+			currentUrl = ct?.url || '';
+			options = { ...options, ...data.options, popupTabId: tabId };
 		}
 
 		getInitialData().catch((e) => console.error(e));
 	});
+
+	function onchange(e: Event) {
+		const target = e.target as HTMLInputElement;
+		const checked = target.checked;
+		if (checked) {
+			options = { ...options, allowedURL: currentUrl };
+		} else {
+			options = { ...options, allowedURL: '' };
+		}
+		chrome.storage.local.set({ options });
+	}
 </script>
 
 <main>
-	{#if currentTab}
-		<div class="text-2xl font-bold underline">{currentTab.url}</div>
+	{#if currentUrl}
+		<div class="text-2xl font-bold underline">{currentUrl}</div>
+
+		<label class="label cursor-pointer">
+			<span class="label-text"
+				>{isClipEnabled
+					? 'Disable clipboard reader'
+					: 'Enable clipboard reader (will clear the clipboard)'}</span
+			>
+			<input type="checkbox" class="toggle toggle-primary" {onchange} checked={isClipEnabled} />
+		</label>
 	{/if}
 </main>
 
